c29b0a862f556769883400ba4e54973dcce4c7ac
robertshum <robertshum@gmail.com>
2024-01-16 15:34:00 -0800

    add node proj dependencies

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello Docker!');
+const app = require('express')();
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Docker is easy to start! 🐳🐋' });
+}
+);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`app is using http://localhost:${port}`));
